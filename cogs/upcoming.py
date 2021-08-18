303a8ba2a73bd1a5a219e5e5ac530abeff163a47
Arjun Muraleedharan <arjunmcd0321@gmail.com>
2021-08-18 20:33:26 +0530

    Added file caching for upcoming

--- cogs/upcoming.py
+++ cogs/upcoming.py
@@ -2,6 +2,7 @@ import discord
 import aiohttp
 import datetime
 import time
+import csv
 from discord.ext import commands
 
 
@@ -13,60 +14,67 @@ class Upcoming(commands.Cog):
     @commands.command()
     async def upcoming(self, ctx):
         async with aiohttp.ClientSession() as session:
-            async with session.get('https://codeforces.com/api/contest.list') as r:
 
-                # If URL was not found
-                if not r.ok:
-                    await ctx.send("Sorry, an error occurred.")
-                    return
+            with open("results.csv") as csvFile:
+                contestList = list(csv.reader(csvFile))
 
-                # Reading the data as JSON data and storing the dictionary in data variable
-                data = await r.json()
+            # Reversing the contest list
+            contestList.reverse()
 
-                # Creating a list to store upcoming contests
-                contestList = []
-                for contest in data["result"]:
-                    if contest["phase"] != "BEFORE":
-                        break
-                    contestList.append(contest)
+            # Creating embed
+            Embed = discord.Embed(title="List of upcoming contests",
+                                  color=0xff0000)
 
-                # Reversing the contest list
-                contestList.reverse()
+            # Adding each contest as a field to the embed
+            for contest in contestList:
 
-                # Creating embed
-                Embed = discord.Embed(title="List of upcoming contests",
-                                      color=0xff0000)
+                # Converting times to int from str
+                contest[2] = int(contest[2])
+                contest[3] = int(contest[3])
 
-                # Adding each contest as a field to the embed
-                for contest in contestList:
+                # Obtaining the time of the contest (dateList[0] -> date, dateList[1] -> time)
+                date = str(datetime.datetime.fromtimestamp(
+                    contest[3]))
+                dateList = date.split()
+                dateList[0] = dateList[0].split("-")
+                dateList[1] = dateList[1].split(":")
 
-                    # Obtaining the time of the contest (dateList[0] -> date, dateList[1] -> time)
-                    date = str(datetime.datetime.fromtimestamp(
-                        contest["startTimeSeconds"]))
-                    dateList = date.split()
-                    dateList[0] = dateList[0].split("-")
-                    dateList[1] = dateList[1].split(":")
+                date = datetime.datetime(int(dateList[0][0]), int(dateList[0][1]), int(
+                    dateList[0][2]), int(dateList[1][0]), int(dateList[1][1]), int(dateList[1][2]))
+                dateString = date.strftime("%b %d, %Y, %H:%M")
 
-                    date = datetime.datetime(int(dateList[0][0]), int(dateList[0][1]), int(
-                        dateList[0][2]), int(dateList[1][0]), int(dateList[1][1]), int(dateList[1][2]))
-                    dateString = date.strftime("%b %d, %Y, %H:%M")
+                # Obtaining contest duration
+                duration = str(datetime.timedelta(
+                    seconds=contest[2]))
+                duration = duration.split(":")
 
-                    # Obtaining contest duration
-                    duration = str(datetime.timedelta(
-                        seconds=contest["durationSeconds"]))
-                    duration = duration.split(":")
+                Embed.add_field(
+                    name=contest[1], value=f"{contest[0]} - {dateString} {time.tzname[0]} - {duration[0]} hrs, {duration[1]} mins", inline=False)
 
-                    Embed.add_field(
-                        name=contest["name"], value=f"{contest['id']} - {dateString} {time.tzname[0]} - {duration[0]} hrs, {duration[1]} mins", inline=False)
+                Embed.set_footer(icon_url=ctx.author.avatar_url,
+                                 text=str(ctx.author))
 
-                    Embed.set_footer(icon_url=ctx.author.avatar_url,
-                                     text=str(ctx.author))
-
-                # Sending embed
-                await ctx.send(embed=Embed)
+            # Sending embed
+            await ctx.send(embed=Embed)
 
     @commands.Cog.listener()
     async def on_ready(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.get('https://codeforces.com/api/contest.list') as r:
+
+                # Reading the contests list as JSON data
+                data = await r.json()
+
+                # Storing the upcoming contests in contests.csv
+                with open("results.csv", "w") as csvFile:
+                    csvWriter = csv.writer(csvFile, delimiter=",")
+                    for c in data['result']:
+                        if c['phase'] == "BEFORE":
+                            csvWriter.writerow(
+                                [c['id'], c['name'], c['durationSeconds'], c['startTimeSeconds']])
+                        else:
+                            break
+
         print("-Upcoming ready!")
 
 
