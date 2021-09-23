fd0581ac0227c548d206d28ad77958e933dc8668
Arjun Muraleedharan <arjunmcd0321@gmail.com>
2021-09-23 23:35:11 +0530

    Minor change

--- cogs/error.py
+++ cogs/error.py
@@ -6,22 +6,22 @@ class Error(commands.Cog):
         self.client = client
 
     # The error handler
-    # @commands.Cog.listener()
-    # async def on_command_error(self, ctx, error):
-    #     message = ''
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        message = ''
 
-    #     if isinstance(error, commands.CommandNotFound):
-    #         message = ":x: The command was not found! Type `-help` to check the list of available commands."
-    #     elif isinstance(error, commands.MissingRequiredArgument):
-    #         message = ":x: One or more arguments are missing!"
-    #     elif isinstance(error, commands.TooManyArguments):
-    #         message = ":x: Too many arguments were provided!"
-    #     else:
-    #         message = ":x: Something went wrong while running the command!"
-    #         print(error)
+        if isinstance(error, commands.CommandNotFound):
+            message = ":x: The command was not found! Type `-help` to check the list of available commands."
+        elif isinstance(error, commands.MissingRequiredArgument):
+            message = ":x: One or more arguments are missing!"
+        elif isinstance(error, commands.TooManyArguments):
+            message = ":x: Too many arguments were provided!"
+        else:
+            message = ":x: Something went wrong while running the command!"
+            print(error)
 
-    #     if message != '':
-    #         await ctx.send(message)
+        if message != '':
+            await ctx.send(message)
 
     @commands.Cog.listener()
     async def on_ready(self):
